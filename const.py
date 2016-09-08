90c4267462f50c88204ba005d7f56fa3a0557f18
Lateef Jackson <lateef.jackson@gmail.com>
2016-09-08 09:11:52 -0700

    Rerun just to update initial findings

--- const.py
+++ const.py
@@ -5,5 +5,5 @@ DB_NAME = 'pqlg_test'
 DB_USER = os.environ['USER']
 NUMBER_QUEUES = 25
 QUEUE_NAMES = ['load_{0}'.format(x) for x in range(NUMBER_QUEUES)]
-GENERATE_SIZE = 10000
+GENERATE_SIZE = 50000
 #GENERATE_SIZE = 1000000
